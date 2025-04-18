eb4fbd52f0157f966b1b11945ad7365f31c59960
Serhii Borshch <72445490+serg1557733@users.noreply.github.com>
2025-04-18 17:54:24 +0200

    add: create nav component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'angular-calc';
-}
+export class AppComponent {}
